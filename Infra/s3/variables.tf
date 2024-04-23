e6fcba48dbfe606e1429f48dd2132dd515c875ec
g-lorena <lorenakouam@gmail.com>
2024-04-23 14:35:29 +0200

    added variables to module s3

--- Infra/s3/variables.tf
+++ Infra/s3/variables.tf
@@ -1,8 +1,18 @@
 
+variable "bucket_name" {
+  description = "principal bucket name"
+  type        = string
+  #default     = "real-estate-etl-101"
+}
 
-
+variable "raw_repertory" {
+  description = "raws data repertory"
+  type        = string
+  #default     = "raw_data"
+}
 
 variable "std_repertory" {
+  description = "std data repertory"
   type        = string
-  default     = "std_data"
+  #default     = "std_data"
 }
