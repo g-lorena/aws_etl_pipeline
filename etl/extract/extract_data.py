963e2f27ec17d530a30424b6dd67fcf61a8b5349
Tedson2019 <sambadzuguia@et.esiea.fr>
2024-04-25 16:26:35 +0200

    update

--- etl/extract/extract_data.py
+++ etl/extract/extract_data.py
@@ -3,7 +3,7 @@ import json
 import requests
 from pathlib import Path
 from datetime import datetime
-#import boto3
+import boto3
 from System.LocalLocation import LocalLocation
 from System.workspace import Workspace
 import pathlib
@@ -25,20 +25,32 @@ QUERY = {"location": "houston, tx"}
 
 
 def lambda_handler(event, context):
-    data = fetch_api_data(API_KEY, API_HOST, URL, QUERY)
     list_workspace_object = create_workspace_objects()
-    date = get_time()[1].split()[0]
+    bucket_name = 'glue-bucket-11794591'
+    date = get_time()[1]
     for workspace_object in list_workspace_object:
-        create_directories(workspace_object)
-    populate_database_table_local(data,date,list_workspace_object)
-    #populate_database_table_s3_bucket(DST_BUCKET,data,date,list_workspace_object)
+        create_s3_directories(bucket_name,workspace_object)
+    
+    #create_directories(workspace_object)
+    #populate_database_table_local(data,date,list_workspace_object)
+    populate_database_table_s3_bucket(bucket_name,date,list_workspace_object)
     
 
+def create_workspace_objects(config_file_path='./extract/system_config.yml'):
+    local = LocalLocation()
+    list_workspace_object = []
+    if  os.path.exists(config_file_path):
+        list_workspace = local.readConfigFile(config_file_path)
+        for workspace in list_workspace:
+            list_workspace_object.append(Workspace(workspace.database, workspace.table_name))
+    return list_workspace_object
 
-def fetch_api_data(api_key, api_host, url, query): 
+
+
+def fetch_api_data(url, query): 
     headers = {
         # mettre api_key et api_host
-        "X-RapidAPI-Key": "ee8d4d9ab3mshbfdc8af6048ab19p12715bjsn3bf8b6f98b0d",
+        "X-RapidAPI-Key": "c7d66d4175msh4b730460e56d07dp177281jsn66cc27e2b144",
         "X-RapidAPI-Host": "zillow56.p.rapidapi.com"
     }
     response = requests.get(url, headers=headers, params=query)
@@ -49,22 +61,62 @@ def fetch_api_data(api_key, api_host, url, query):
     else:
         raise Exception(f'Error fetching data: {response.text}')
 
+def create_s3_directories(bucket_name,workspace_object):
+    s3 = boto3.client('s3')
+    database_name_s3 = workspace_object.get_database()
+    table_name_s3 = workspace_object.get_table_name()
+    database_name_s3_prefix = str(database_name_s3)
+    table_name_s3_prefix = str(database_name_s3) + "/" + str(table_name_s3)
+    try:
+        s3.put_object(Bucket=bucket_name, Key=(database_name_s3_prefix + '/'))
+        s3.put_object(Bucket=bucket_name,  Key=(table_name_s3_prefix + '/'))
+    except s3.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == '404':
+            pass 
+
+
+def populate_database_table_s3_bucket(bucket_name,date,list_workspace_object):
+    query = None   
+    url = "https://zillow56.p.rapidapi.com/search"
+
+    s3 = boto3.client('s3', region_name="eu-west-3") 
+    cpt = 0
+    while cpt < len(list_workspace_object):
+        try:
+            workspace_object = None 
+            workspace_object = list_workspace_object[cpt]
+            database_name = workspace_object.get_database()
+            table_name = workspace_object.get_table_name()
+            file_name = f"{table_name}_{date}.json"
+            query =  {"location": f"{table_name}, tx"}
+            #data = fetch_api_data(url, query)
+            data = {"test":"alla"}
+            s3_object_key = f"{database_name}/{table_name}/{date}/{file_name}" 
+            # Convert data to a byte stream (assuming it's serializable)
+            if isinstance(data, dict):
+                data_bytes = json.dumps(data, ensure_ascii=False).encode('utf-8')
+            else:
+                raise TypeError("Data must be a dictionary serializable to JSON")
+            try:
+                s3.put_object(Bucket=bucket_name,Key=s3_object_key,Body=data_bytes)
+                cpt += 1
+            except ClientError as e:
+                raise Exception(f"Error uploading data to S3: {e}") from e  # Re-raise with more context
+        except Exception as e:
+            print(f"Error populating table '{table_name}': {e}")
+
 def get_time():  
     dt = datetime.now()
     timestamp = str(datetime.timestamp(dt)).replace('.', '_')
-    return timestamp,dt
-
-def create_workspace_objects(config_file_path='/Users/tedsamba/Desktop/GLUE/aws_etl_pipeline/etl/extract/system_config.yml'):
-    local = LocalLocation()
-    list_workspace_object = []
-    if  os.path.exists(config_file_path):
-        list_workspace = local.readConfigFile(config_file_path)
-        for workspace in list_workspace:
-            list_workspace_object.append(Workspace(workspace.database, workspace.table_name))
-    return list_workspace_object
+    return timestamp,dt.strftime('%Y-%m-%d')
 
 
-def create_directories(workspace_object):
+
+
+
+
+
+def create_local_directories(workspace_object):
     database_name = workspace_object.get_database()
     table_name = workspace_object.get_table_name()
     if not os.path.exists(database_name):
@@ -90,26 +142,6 @@ def populate_database_table_local(data,date,list_workspace_object):
             print(f"Error populating table '{table_name}': {e}")
 
 
-def populate_database_table_s3_bucket(bucket_name,data,date,list_workspace_object):  
-    s3_client = boto3.client('s3', region_name=region_name) 
-    for workspace_object in list_workspace_object:
-        try:
-            database_name = workspace_object.get_database()
-            table_name = workspace_object.get_table_name()
-            file_name = f"{table_name}_{date}.json"
-            s3_object_key = f"{database_name}/{table_name}/{date}/{file_name}" 
 
-            # Convert data to a byte stream (assuming it's serializable)
-            if isinstance(data, dict):
-                data_bytes = json.dumps(data, ensure_ascii=False).encode('utf-8')
-            else:
-                raise TypeError("Data must be a dictionary serializable to JSON")
-            try:
-                s3_client.upload_fileobj(data_bytes, bucket_name, s3_object_key)
-            except ClientError as e:
-                raise Exception(f"Error uploading data to S3: {e}") from e  # Re-raise with more context
-
-        except Exception as e:
-            print(f"Error populating table '{table_name}': {e}")
 
 lambda_handler(None, None)
